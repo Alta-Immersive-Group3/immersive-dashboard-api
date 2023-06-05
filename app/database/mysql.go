ad4d9cf773764b01939282a3e6b467c851235cf3
belhiibeng <belhi.romdona@gmail.com>
2023-06-05 16:40:18 +0700

    first commit

---
+++ app/database/mysql.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/ALTA-Immersive-Group3/immersive-dahsboard-api/app/config"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDBMysql(cfg *config.AppConfig) *gorm.DB {
+
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+		cfg.DB_USERNAME, cfg.DB_PASSWORD, cfg.DB_HOSTNAME, cfg.DB_PORT, cfg.DB_NAME)
+
+	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
