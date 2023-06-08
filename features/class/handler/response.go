c800fcc86b30acf4aa39ec3bcf788a4376822cf5
belhiibeng <belhi.romdona@gmail.com>
2023-06-08 11:14:17 +0700

    feature: class service

--- features/class/handler/response.go
+++ features/class/handler/response.go
@@ -1,1 +1,23 @@
 package handler
+
+import (
+	"github.com/ALTA-Immersive-Group3/immersive-dahsboard-api/features/class"
+)
+
+type ClassResponse struct {
+	Id           uint64 `json:"id" form:"id"`
+	Name         string `json:"name" form:"name"`
+	PIC          uint64 `json:"pic" form:"pic"`
+	StartDate    string `json:"start_date" form:"start_date"`
+	GraduateDate string `json:"graduate_date" form:"graduate_date"`
+}
+
+func CoreToGetClassResponse(class class.Core) ClassResponse {
+	return ClassResponse{
+		Id:           class.Id,
+		Name:         class.Name,
+		PIC:          class.PIC,
+		StartDate:    class.StartDate,
+		GraduateDate: class.GraduateDate,
+	}
+}
--- features/class/service/logic.go
+++ features/class/service/logic.go
@@ -1,1 +1,66 @@
 package service
+
+import (
+	"github.com/ALTA-Immersive-Group3/immersive-dahsboard-api/features/class"
+	"github.com/go-playground/validator/v10"
+)
+
+type classService struct {
+	classData class.ClassDataInterface
+	validate  *validator.Validate
+}
+
+func New(repo class.ClassDataInterface) class.ClassServiceInterface {
+	return &classService{
+		classData: repo,
+		validate:  validator.New(),
+	}
+}
+
+func (service *classService) Create(input class.Core) (class.Core, error) {
+	errValidate := service.validate.Struct(input)
+	if errValidate != nil {
+		return class.Core{}, errValidate
+	}
+
+	id, errInsert := service.classData.Insert(input)
+	if errInsert != nil {
+		return class.Core{}, errInsert
+	}
+
+	return service.GetById(id)
+}
+
+func (service *classService) GetAll() ([]class.Core, error) {
+	data, err := service.classData.SelectAll()
+	if err != nil {
+		return nil, err
+	}
+	return data, err
+}
+
+func (service *classService) GetById(id uint64) (class.Core, error) {
+	data, err := service.classData.SelectById(id)
+	if err != nil {
+		return class.Core{}, err
+	}
+	return data, err
+}
+
+func (service *classService) UpdateById(id uint64, input class.Core) (class.Core, error) {
+	errUpdate := service.classData.UpdateById(id, input)
+	if errUpdate != nil {
+		return class.Core{}, errUpdate
+	}
+
+	return service.GetById(id)
+}
+
+func (service *classService) DeleteById(id uint64) error {
+	errUpdate := service.classData.DeleteById(id)
+	if errUpdate != nil {
+		return errUpdate
+	}
+
+	return nil
+}
